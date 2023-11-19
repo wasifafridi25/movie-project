b112198728019feec931873ea8991e1cd128bb4e
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-11-19 05:25:55 -0800

    Created the custom hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+//default export don't need curly braces
+import useAuth from "../hooks/useAuth";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  //if have arrow function can use directly but this function is returning something so need to store in variable 
+  const AuthProvider = useAuth;
+  return (
+    
+    //HOC - Higher order component
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,32 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 export default function login() {
+  const [login, setLogin] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+      // await signIn(email, password)
+    } else {
+      // await signUp(email, password)
+    }
+  };
+
   return (
     <div className="relative h-screen w-full flex flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
-        <title>Home - Netflix</title>
+        <title>Netflix</title>
       </Head>
       <Image
         alt="netflix banner"
@@ -21,23 +42,56 @@ export default function login() {
         className="absolute top-4 left-4 cursor-pointer object-contain md:left-10 md:top-6"
       />
 
-      <form action="" className="relative bg-black/75 mt-24 py-10 px-6 rounded space-y-8 md:mt-0 md:px-14
-      md:max-w-md">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative bg-black/75 mt-24 py-10 px-6 rounded space-y-8 md:mt-0 md:px-14
+      md:max-w-md"
+      >
         <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="flex flex-col space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input"/>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input"/>
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
-        <button type="submit" className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          onClick={() => setLogin(true)}
+          type="submit"
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+        >
           Sign In
         </button>
-        <div>
-          New to Netflix?
-          <button type="submit" className="hover:underline">Sign up now</button>
+        <div className="text-[gray]">
+          New to Netflix?{" "}
+          <button
+            onClick={() => setLogin(false)}
+            type="submit"
+            className="text-white hover:underline"
+          >
+            Sign up now
+          </button>
         </div>
       </form>
     </div>
